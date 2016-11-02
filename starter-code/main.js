c82fd0a50c52eb0213b3d4acfa0972ea4aafc8d5
sherpaguy <sonamtorres@Kunjang-Sherpa.PK5001Z>
2016-11-02 14:53:35 -0700

    created alert()

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,12 @@ var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+if (cardTwo === cardFour) {
+	alert("Sorry, try again.");
+} else if (cardOne === cardTwo){
+	alert("You found a match!");
+} else if (cardOne === cardThree){
+	alert("Sorry, try again.");
+}else if (cardThree === cardFour){
+	alert("You found a match!")
+}
