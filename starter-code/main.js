d7e255c3356cc79863ebf2d817e6c9d5ab83d34a
sherpaguy <sonamtorres@Kunjang-Sherpa.local>
2016-11-28 07:26:14 -0800

    created memory-game

--- starter-code/main.js
+++ starter-code/main.js
@@ -29,4 +29,40 @@ function createBoard() {
     board.appendChild(cardElement);
   }
 
+};
+
+//checks to see if there are cards in play
+function isTwoCards() {
+  // add card to array of cards being viewed
+  // 'this' is not covered in the pre-work but
+  // for now, just know it gives you access to the cardElement you click on
+  cardsInPlay.push(this.getAttribute('data-card'));
+    // show the card's image
+    console.log(this.getAttribute('data-card'));
+    if (this.getAttribute('data-card') === 'king') {
+        this.innerHTML = "<img src = 'king.png'>"; // king
+    } else {
+        this.innerHTML = "<img src='queen.png'>"; //queen
+    }
+  // if you have two cards in play check for a match
+  if (cardsInPlay.length === 2) {
+    // pass the cardsInPlay as an argument to isMatch function
+    isMatch(cardsInPlay);
+    // clear cards in play array for next try
+    cardsInPlay = [];
+  }
 }
+
+function isMatch(cards) {
+  // alert winning message
+  if (cards[0] === cards[1]) {
+    alert("You found a match!");
+  } else {
+    alert("Sorry, try again.");
+
+  }
+}
+
+createBoard();
+
+
