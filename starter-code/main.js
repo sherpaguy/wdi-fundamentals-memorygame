6210cf8262d3dd27233c5c6cbf0bca1c8801a638
sherpaguy <sonamtorres@Kunjang-Sherpa.local>
2016-11-28 07:22:09 -0800

    created game logic

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,13 +1,32 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-if (cardTwo === cardFour) {
-	alert("Sorry, try again.");
-} else if (cardOne === cardTwo){
-	alert("You found a match!");
-} else if (cardOne === cardThree){
-	alert("Sorry, try again.");
-}else if (cardThree === cardFour){
-	alert("You found a match!")
+// represents the cards of the game
+var cards = ['queen', 'queen', 'king', 'king'];
+
+// represents the cards in play
+var cardsInPlay = [];
+
+
+
+// find the board and set it to a variable
+// so that we can add cards to it
+var board = document.getElementById('game-board');
+// function that will create your board
+function createBoard() {
+  // loop through your cards array to create card elements for your board
+  for (var i=0; i<cards.length; i++) {
+    // create a div element which will be used as a card
+    var cardElement = document.createElement('div');
+    // add a class to the card element which will help link styling
+    cardElement.className = 'card';
+        // this will set the card's 'data-card' to be the element of the array
+    // data- attributes are meant to store data about an element that is not tied to a style.
+    // i.e. "king"
+    cardElement.setAttribute('data-card', cards[i]);
+        // when a card is clicked the function isTwoCards will be executed
+    cardElement.addEventListener('click', isTwoCards);
+
+    // append the card to the board
+    board.appendChild(cardElement);
+    board.appendChild(cardElement);
+  }
+
 }
